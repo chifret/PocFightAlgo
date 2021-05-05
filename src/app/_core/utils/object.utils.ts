bfe0cd45e8d687d9dfb6539f1e8e461b96fa3b89
chifret <chifret@gmail.com>
2021-05-05 22:26:22 +0200

    Update object.utils.ts

--- src/app/_core/utils/object.utils.ts
+++ src/app/_core/utils/object.utils.ts
@@ -2,10 +2,9 @@ import * as fightEvents from '../classes/fight/actions/_lib-actions-fight.class'
 import * as buffEvents from '../classes/fight/buffs/_lib-buff-fight.class';
 import {ActionFightClass} from '../classes/fight/action-fight.class';
 import {BuffFightClass} from '../classes/fight/buff-fight.class';
-import {Injector, Type} from '@angular/core';
-import {BeforemoveBuffFightInterface} from '../interfaces/fight/buffs/beforemove-buff-fight.interface';
+import {Injector} from '@angular/core';
 
-export class ObjectUtils<T> {
+export class ObjectUtils {
 
   static getEventInstance(name: string, injector: Injector): ActionFightClass {
     // @ts-ignore
